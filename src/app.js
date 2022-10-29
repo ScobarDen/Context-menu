0fd8e018aafa81932128f6ab8ef3a41693ec91cf
Dmitriy Emelyanov <harnblaze@gmail.com>
2022-10-29 18:49:45 +0300

    добавил модуль для рисования

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
-import './styles.css'
-import {ContextMenu} from "@/menu";
+import "./styles.css";
+import { ContextMenu } from "@/menu";
+import { Paint } from "@/modules/paint";
 
-const contextMenu = new ContextMenu()
+const contextMenu = new ContextMenu();
+const paint = new Paint();
+contextMenu.add(paint);
