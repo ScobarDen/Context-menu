3fa5752b6b135059b2157c359fc0eb09ae92a3ea
aleksei_volkov_2 <ptolome1984@gmail.com>
2022-10-30 08:46:10 +0300

    added module sound

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@ import './styles.css'
 import {ContextMenu} from "@/menu";
 import {ClicksModule} from "@/modules/clicks.module";
 import {BackgroundModule} from "@/modules/background.module";
+import {SoundModule} from "@/modules/sound.module";
 
 const contextMenu = new ContextMenu();
 const background = new BackgroundModule();
 const clicks = new ClicksModule(5);
-contextMenu.add(background)
+const sounds = new SoundModule();
+contextMenu.add(background);
 contextMenu.add(clicks);
+contextMenu.add(sounds)
